540261fcf896ca1169b8304c18f79eef0f057c8c
Carlassanchez24 <carlassanchez24@gmail.com>
2024-10-28 12:08:06 +0100

    feat: add more conditionasl

--- src/main/java/org/example/Conditionals.java
+++ src/main/java/org/example/Conditionals.java
@@ -4,16 +4,21 @@ public class Conditionals {
     public static void main(String[] args) {
         boolean isActive = false;
 
-       // imprimir el user activo si esta activo
-        if (isActive){
+        if (isActive) {
             System.out.println("El usuario esta activo");
-        }
-        else {
+        } else {
             System.out.println("El usuario no esta activo");
 
         }
-        // imprimir el user activo si esta activo
 
+        int edad = 9;
+        if (edad >= 18) {
+            System.out.println("El usuario es mayor de edad");
+        } else {
+            System.out.println("El usuario no es mayor de edad");
+
+        }
 
     }
+
 }
