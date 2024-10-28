3fc5c40a6461707b5dfab15ee4125cc34986964c
Carlassanchez24 <carlassanchez24@gmail.com>
2024-10-28 10:52:10 +0100

    feat: add types like float and double

--- src/main/java/org/example/BasicTypes.java
+++ src/main/java/org/example/BasicTypes.java
@@ -1,4 +1,28 @@
 package org.example;
 
 public class BasicTypes {
+    public static void main(String[] args) {
+
+        //escribir boolean, asignarlo, mostrar por consola
+        boolean isActive = false;
+
+        System.out.println(isActive);
+
+        // numeros enteros
+
+        byte edad = 24;
+        System.out.println(edad);
+        short año =2024;
+        System.out.println(año);
+        int habitantes = 10000;
+        System.out.println(habitantes);
+        long distancialuna = 123456789L;
+        System.out.println(distancialuna);
+        float precio = 24.9912f;
+        System.out.println(precio);
+        double pi = 3.142245335;
+        System.out.println(pi);
+
+    }
 }
+
