acfc697ba8c49907b5817212d0c4ba7c7abcb49c
Carlassanchez24 <carlassanchez24@gmail.com>
2024-10-28 12:47:36 +0100

    bug: fix bug

--- src/main/java/org/example/ForLoop.java
+++ src/main/java/org/example/ForLoop.java
@@ -3,8 +3,14 @@ package org.example;
 public class ForLoop {
     public static void main(String[] args) {
         //imprimir por consola los numeros de 5 a 10
-        for (int i=5; i <= 10; i ++) {
+        for (int i = 5; i <= 10; i++) {
             System.out.println(i);
+        }
+        //imprimir por consola los numeros pares de 5 a 10
+        for (int i = 5; i <= 10; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+
         }
     }
 }
